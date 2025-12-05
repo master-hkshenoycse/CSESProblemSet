28688dea9e7d456b8afd3dee761e8de7f732fea1
master-hkshenoycse <hkshenoy.dev@gmail.com>
2025-12-05 23:16:35 +0530

    IntroductoryProblems: Permutations.cpp: Implement solution
    Constructive algo based problem, first print even numbers then odd,
    handle corner cases.

---
+++ IntroductoryProblems/Permutations.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ll n;
+    cin>>n;
+
+    if(n==1){
+        cout<<1<<endl;
+        return 0;
+    }
+
+    if(n==2 ||  n==3){
+        cout<<"NO SOLUTION"<<endl;
+        return 0;
+    }
+
+    for(ll i=2;i<=n;i+=2){
+        cout<<i<<" ";
+    }
+
+    for(ll i=1;i<=n;i+=2){
+        cout<<i<<" ";
+    }
+
+    cout<<endl;
+}
